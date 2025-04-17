bfd599933266776e32ababbe6129770e59bc31a8
likevanilla <wjdgur27507@gmail.com>
2025-04-17 23:43:44 +0900

    n5 해결

--- LeeJeonghyuk/HW/n5.js
+++ LeeJeonghyuk/HW/n5.js
@@ -1,1 +1,13 @@
+function solution() {
+    let color = ["Red", "Orange", "Yellow", "Green", "Blue", "Navy", "Indigo"];
 
+    color.push("Babyblue");
+    color.push("Pink");
+
+    console.log(color.splice(3, 1, "Lightgreen"));
+
+    console.log(color);
+    console.log(color.length);
+}
+
+solution();
