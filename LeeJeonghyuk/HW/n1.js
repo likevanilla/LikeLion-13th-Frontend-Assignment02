67c49a2b4724992c0ef3d520ef0b43001c7cd020
likevanilla <wjdgur27507@gmail.com>
2025-04-17 22:58:05 +0900

    n1 해결

--- LeeJeonghyuk/HW/n1.js
+++ LeeJeonghyuk/HW/n1.js
@@ -1,1 +1,20 @@
+function solution(n) {
+    let starPyramid = '';
 
+    for(let i = 0; i < n; i++) {
+        for(let j = 0; j < n - i; j++) {
+            starPyramid += ' ';
+        }
+
+        for(let k = 0; k < 2 * i + 1; k++) {
+            starPyramid += '*';
+        }
+        
+        starPyramid += '\n';
+    }
+
+    console.log(starPyramid);
+}
+
+solution(3);
+solution(5);
