bb584abcb430ebcfeb2a1a1366aa96d57a690d1e
likevanilla <wjdgur27507@gmail.com>
2025-04-17 23:26:52 +0900

    n4 해결

--- LeeJeonghyuk/HW/n4.js
+++ LeeJeonghyuk/HW/n4.js
@@ -1,1 +1,25 @@
+function solution(num1, num2) {
 
+    console.log("덧셈: " + add(num1, num2));
+    console.log("뺄셈: " + sub(num1, num2));
+    console.log("나눗셈: " + div(num1, num2));
+    console.log("곱셈: " + mul(num1, num2));
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function sub(num1, num2) {
+    return num1 - num2;
+}
+
+function div(num1, num2) {
+    return num1 / num2;
+}
+
+function mul(num1, num2) {
+    return num1 * num2;
+}
+
+solution(1, 2);
