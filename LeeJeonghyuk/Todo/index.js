40d68628715148a62a63ce103e6e94c21ef37379
likevanilla <wjdgur27507@gmail.com>
2025-04-27 18:51:50 +0900

    리스트 간 구분선 넣기 및 전체적인 디자인 변경

--- LeeJeonghyuk/Todo/index.js
+++ LeeJeonghyuk/Todo/index.js
@@ -15,8 +15,12 @@ function renderTodos() {
     list.innerHTML = "";
     todos.forEach((todo, index) => {
         const li = document.createElement("li");
-        li.textContent = todo;
-        li.onclick = () => removeTodo(index);
+        //js 2번 조건
+        const checkbox = document.createElement("input");
+        checkbox.setAttribute("type", "checkbox");
+        checkbox.textContent = todo;
+        li.append(checkbox, checkbox.textContent);
+        // li.onclick = () => removeTodo(index);
         list.appendChild(li);
     });
 }
@@ -24,4 +28,11 @@ function renderTodos() {
 function removeTodo(index) {
     todos.splice(index, 1);
     renderTodos();
+}
+
+//js 1번 조건
+function enterKey() {
+    if (window.event.keyCode == 13) {
+        addTodo();
+    }
 }
